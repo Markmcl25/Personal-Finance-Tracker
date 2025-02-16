d782a415550c414c5d52a937140d0629e1318cb7
Mark Mcloughlin <mark25mky@gmail.com>
2025-02-16 01:10:00 +0000

    created dashboard and populated

--- finance_tracker/finance_tracker/settings.py
+++ finance_tracker/finance_tracker/settings.py
@@ -33,10 +33,11 @@ ALLOWED_HOSTS = ['localhost',
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker',
