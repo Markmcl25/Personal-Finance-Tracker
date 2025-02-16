779918bafb4ee064ed2fda69c6c10fd1c5c461f2
Mark Mcloughlin <mark25mky@gmail.com>
2025-02-16 00:34:25 +0000

    created tracker app and updated models

---
+++ finance_tracker/tracker/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title} - {self.amount}"
+
