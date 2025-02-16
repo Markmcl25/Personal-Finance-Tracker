0d41f67445e250376afd6338deac65ffc1890cec
Mark Mcloughlin <mark25mky@gmail.com>
2025-02-16 00:58:10 +0000

    updated views for sign up view

--- finance_tracker/tracker/views.py
+++ finance_tracker/tracker/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import SignupForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            login(request, user)  # Log user in after signup
+            return redirect('dashboard')  # Redirect to dashboard
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
