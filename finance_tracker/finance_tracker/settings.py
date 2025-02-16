f2b29f3de970cd9f2b3c02feebc06d69733afb1e
Mark Mcloughlin <mark25mky@gmail.com>
2025-02-16 21:07:17 +0000

    Fixed tracker app path and imports

--- finance_tracker/finance_tracker/settings.py
+++ finance_tracker/finance_tracker/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'finance_tracker.tracker',  # Use full dotted path
+    'tracker',
 ]
 
 MIDDLEWARE = [
